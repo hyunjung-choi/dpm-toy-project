97b4cd5563adcc61d40387daa0e3b635920d6761
hyunjung-choi <hjchoi.dev@gmail.com>
2021-04-06 23:07:25 +0900

    dividerItemDecoration 추가

--- app/src/main/java/com/hyunjung/toyprojectapp/contact/ContactFragment.kt
+++ app/src/main/java/com/hyunjung/toyprojectapp/contact/ContactFragment.kt
@@ -1,34 +1,38 @@
 package com.hyunjung.toyprojectapp.contact
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
-import com.hyunjung.toyprojectapp.R
 import com.hyunjung.toyprojectapp.adapter.ContactAdapter
 import com.hyunjung.toyprojectapp.databinding.FragmentContactBinding
 import org.json.JSONObject
 import java.io.FileNotFoundException
+
 
 class ContactFragment : Fragment() {
 
     lateinit var binding : FragmentContactBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentContactBinding.inflate(inflater, container,false)
+        binding = FragmentContactBinding.inflate(inflater, container, false)
 
         val contactData : MutableList<Contact> = loadContactData()
-        var contactAdapter = ContactAdapter()
+        val contactAdapter = ContactAdapter()
         contactAdapter.contactData = contactData
         binding.contactRv.adapter = contactAdapter
         binding.contactRv.layoutManager = LinearLayoutManager(this.context)
+
+        // 구분선
+        val dividerItemDecoration = DividerItemDecoration(binding.contactRv.getContext(), LinearLayoutManager(this.context).orientation)
+        binding.contactRv.addItemDecoration(dividerItemDecoration)
 
         return binding.root
     }
