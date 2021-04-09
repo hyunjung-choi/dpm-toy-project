18122637e93d96cba966d2074e932c8113a1202b
hyunjung-choi <hjchoi.dev@gmail.com>
2021-04-09 20:40:48 +0900

    연락처 추가 기능 구현

--- app/src/main/java/com/hyunjung/toyprojectapp/contact/ContactFragment.kt
+++ app/src/main/java/com/hyunjung/toyprojectapp/contact/ContactFragment.kt
@@ -1,13 +1,19 @@
 package com.hyunjung.toyprojectapp.contact
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
+import com.hyunjung.toyprojectapp.R
 import com.hyunjung.toyprojectapp.adapter.ContactAdapter
 import com.hyunjung.toyprojectapp.databinding.FragmentContactBinding
 import org.json.JSONObject
@@ -16,12 +22,12 @@ import java.io.FileNotFoundException
 
 class ContactFragment : Fragment() {
 
-    lateinit var binding : FragmentContactBinding
+    private lateinit var binding : FragmentContactBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+        ): View {
         binding = FragmentContactBinding.inflate(inflater, container, false)
 
         val contactData : MutableList<Contact> = loadContactData()
@@ -31,13 +37,49 @@ class ContactFragment : Fragment() {
         binding.contactRv.layoutManager = LinearLayoutManager(this.context)
 
         // 구분선
-        val dividerItemDecoration = DividerItemDecoration(binding.contactRv.getContext(), LinearLayoutManager(this.context).orientation)
+        val dividerItemDecoration = DividerItemDecoration(binding.contactRv.context, LinearLayoutManager(this.context).orientation)
         binding.contactRv.addItemDecoration(dividerItemDecoration)
+
+        // FAB 클릭 시, 다이얼로그 생성
+        binding.contactFabAdd.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this.context).inflate(R.layout.dialog_add, null)
+            val mBuilder = AlertDialog.Builder(this.context)
+                    .setView(mDialogView)
+
+            val mAlterDialog = mBuilder.show()
+
+            val btnOk = mDialogView.findViewById<Button>(R.id.dialog_btn_ok)
+            val btnCancel = mDialogView.findViewById<Button>(R.id.dialog_btn_cancel)
+
+            val etName = mDialogView.findViewById<EditText>(R.id.dialog_et_name)
+            val etAge = mDialogView.findViewById<EditText>(R.id.dialog_et_age)
+            val etCountry = mDialogView.findViewById<EditText>(R.id.dialog_et_country)
+            val etPhone = mDialogView.findViewById<EditText>(R.id.dialog_et_phone)
+
+            // 연락처 자동 하이픈 입력
+            etPhone.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+
+            btnOk.setOnClickListener {
+                val name = etName.text.toString()
+                val age = etAge.text.toString().toInt()
+                val phone = etPhone.text.toString()
+                val country = etCountry.text.toString()
+
+                contactData.add(Contact(name, age, phone, country))
+
+                mAlterDialog.dismiss()
+                Toast.makeText(this.context, "연락처가 등록되었습니다.", Toast.LENGTH_LONG).show()
+            }
+
+            btnCancel.setOnClickListener {
+                mAlterDialog.dismiss()
+            }
+        }
 
         return binding.root
     }
 
-    fun loadContactData() : MutableList<Contact>{
+    private fun loadContactData() : MutableList<Contact>{
         val data: MutableList<Contact> = mutableListOf()
         val gson = Gson()
         val testString: String
@@ -65,3 +107,4 @@ class ContactFragment : Fragment() {
     }
 
 }
+
