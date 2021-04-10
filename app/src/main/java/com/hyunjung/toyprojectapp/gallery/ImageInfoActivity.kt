f3e31914ecbc540a6f073dded2570c2788f88d58
hyunjung-choi <hjchoi.dev@gmail.com>
2021-04-10 19:26:05 +0900

    화면 회전 기능 구현

--- app/src/main/java/com/hyunjung/toyprojectapp/gallery/ImageInfoActivity.kt
+++ app/src/main/java/com/hyunjung/toyprojectapp/gallery/ImageInfoActivity.kt
@@ -1,13 +1,21 @@
 package com.hyunjung.toyprojectapp.gallery
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.hyunjung.toyprojectapp.databinding.ActivityImageInfoBinding
+
 
 class ImageInfoActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityImageInfoBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,14 @@ class ImageInfoActivity : AppCompatActivity() {
         Glide.with(this).load(image).into(binding.infoIvImage)
         binding.infoTvAuthor.text = "Author: ${author}"
         binding.infoTvSize.text = "Size: ${width}x${height}"
+
+        binding.infoBtnRotation.setOnClickListener {
+            val orientation = this.resources.configuration.orientation
+
+            when(orientation){
+                Configuration.ORIENTATION_PORTRAIT -> requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                Configuration.ORIENTATION_LANDSCAPE -> requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
     }
 }
