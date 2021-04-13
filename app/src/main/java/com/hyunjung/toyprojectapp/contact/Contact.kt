3462e9838e6ce950e7926285220b3444f9a3ecc3
hyunjung-choi <hjchoi.dev@gmail.com>
2021-04-14 08:50:05 +0900

    Contact.kt 변수 val로 변경

--- app/src/main/java/com/hyunjung/toyprojectapp/contact/Contact.kt
+++ app/src/main/java/com/hyunjung/toyprojectapp/contact/Contact.kt
@@ -1,4 +1,8 @@
 package com.hyunjung.toyprojectapp.contact
 
-data class Contact(var Name: String, var age: Int, var PhoneNum: String, var Country: String) {
-}
+data class Contact(
+        val Name: String,
+        val age: Int,
+        val PhoneNum: String,
+        val Country: String
+        )
